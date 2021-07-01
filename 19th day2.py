27f26cb70041f3107bd79ff62f94c24f1314442a
Ketan Gurav <54971822+itzketan@users.noreply.github.com>
2021-07-01 21:42:49 +0530

    Add files via upload

--- 19th day2.py
+++ 19th day2.py
@@ -9,12 +9,12 @@ dbase = mydb.cursor()
 dbase.execute("SHOW DATABASES")
 for i in dbase:
     print(i)
-dbase.execute("CREATE TABLE Student6(Addmission_no VARCHAR(225),First_Name VARCHAR(20), Surname VARCHAR(20), DOB int(10), Gender VARCHAR(6) , Year int(3))")
+dbase.execute("CREATE TABLE Student7(Addmission_no VARCHAR(225),First_Name VARCHAR(20), Surname VARCHAR(20), DOB int(10), Gender VARCHAR(6) , Year int(3))")
 dbase.execute("SHOW TABLES")
 for table in dbase:
     print(table)
 for row in range(1,sheet.utter_max_rows):
-    sql = "INSERT INTO Student6(Addmission_no, First_Name, Surname, DOB, Gender, Year)VALUES (%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO Student7(Addmission_no, First_Name, Surname, DOB, Gender, Year)VALUES (%s,%s,%s,%s,%s,%s)"
     for _ in range(0):
         Addmission_no = sheet.cell(row,0).value
         First_Name = sheet.cell(row,1).value
@@ -26,8 +26,20 @@ for row in range(1,sheet.utter_max_rows):
         values = (Addmission_no, First_Name, Surname, DOB, Gender, Year)
         dbase.execute(sql, values)
 mydb.commit()
-dbase.execute("SELECT * FROM Student6")
+dbase.execute("SELECT * FROM Student7")
 database = dbase.fetchall()
 for data in database:
     print(data)
 mydb.close()
+import mysql.connector
+
+mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="@mysqlketangurav22",
+   database="Studentdata"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute('SELECT * FROM student7')
